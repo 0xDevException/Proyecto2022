8cc92756ac311c07d910734e6e436a52442b3c23
romitus <alum.cmunozi@iesalixar.org>
2022-10-22 18:29:38 +0200

    create update delete Completed

--- src/controllers/UserController.py
+++ src/controllers/UserController.py
@@ -15,8 +15,33 @@ def store():
 def show(userId):
     ...
 
+def create():
+    if not request.json:
+        abort(400)
+    user = User(
+        name=request.json.get('name'),
+        city=request.json.get('city'),
+        state=request.json.get('state'),
+        address=request.json.get('address')
+    )
+    db.session.add(user)
+    db.session.commit()
+
 def update(userId):
-    ...
+    if not request.json:
+        abort(400)
+    user = User.query.get(userId)
+    if user is None:
+        abort(404)
+    user.name = request.json.get('name', user.name)
+    user.city = request.json.get('city', user.city)
+    user.state = request.json.get('state', user.state)
+    user.address = request.json.get('address', user.address)
+    db.session.commit()
 
 def delete(userId):
-    ...
+    user = User.query.get(userId)
+    if user is None:
+        abort(404)
+    db.session.delete(user)
+    db.session.commit()
