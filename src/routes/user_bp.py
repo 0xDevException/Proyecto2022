985788df72c7dfdc441b214bf7bd7f0d852c6640
romitus <alum.cmunozi@iesalixar.org>
2022-10-22 18:33:01 +0200

    Makes available methods update delete an create

--- src/routes/user_bp.py
+++ src/routes/user_bp.py
@@ -1,11 +1,11 @@
 from flask import Blueprint
 
-from controllers.UserController import index#, store, show, update, destroy
+from controllers.UserController import index, update, delete, create #,show
 
 user_bp = Blueprint('user_bp', __name__)
 
 user_bp.route('/', methods=['GET'])(index)
-#user_bp.route('/create', methods=['POST'])(store)
+user_bp.route('/create', methods=['POST'])(create)
 #user_bp.route('/<int:user_id>', methods=['GET'])(show)
-#user_bp.route('/<int:user_id>/edit', methods=['POST'])(update)
-#user_bp.route('/<int:user_id>', methods=['DELETE'])(destroy)
+user_bp.route('/<int:user_id>/edit', methods=['POST'])(update)
+user_bp.route('/<int:user_id>', methods=['DELETE'])(delete)
